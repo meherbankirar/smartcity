040fc0bba91208d09f122ddf18a7f77e63adabc0
meherban <meherankirar@gmail.com>
2022-03-31 22:54:57 +0530

    initial commit

---
+++ src/main/java/com/example/Smartcity/SmartcityApplication.java
@@ -0,0 +1,13 @@
+package com.example.Smartcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Smartcity/SmartcityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Smartcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
